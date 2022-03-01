fd37a4a5ca4dbcf60efd6989bccf9e7d736941c9
AmyFong82 <amyhfong@gmail.com>
2022-03-01 10:14:33 -0500

    Exporting as a single function.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 //used const to set the variable for a required file
 //so that we don't overwrite it.
-const logger = require('./logger')
+const log = require('./logger')
 
 //see what logger is in the console.
-console.log(logger)
-//it returns this, it has a function called log
+console.log(log)
+//when it's exported as an object, it looks like this:
+// It has a function called log
 // { log: [Function: log] }
 
+//When it's exported as a single function, it looks like this:
+// [Function: log]
+
 // try it out
-logger.log("Amy, how are you!")
+log("Amy, how are you!")
--- logger.js
+++ logger.js
@@ -7,4 +7,8 @@ function log(msg) {
 
 //This is how to export something inside a module.
 // A js file in node is a module.
+//Exporting as an object:
 module.exports.log = log
+
+//Exporting as a single function:
+module.exports = log
